95335aa4fcd271bc60d8e24a317ebabc63dd71ad
KartiKBriteinno <153173760+KartiKBriteinno@users.noreply.github.com>
2024-03-15 21:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 function isSameType(value1, value2) {
-  if(typeof(value1)==typeof(value2)){
-		return true;
-	}
-	else if (value1===value2){
-		return true;
-	}
-	else{
-		return false;
-	}
+    // Check if both values are of the same type (number, string, or NaN)
+    if ((typeof value1 === typeof value2) || (isNaN(value1) && isNaN(value2))) {
+        return true;
+    } else {
+        return false;
+    }
 }
+
+// Test cases
+console.log(isSameType(1, 3)); // true
+console.log(isSameType("hey", "hello")); // true
+console.log(isSameType(NaN, NaN)); // true
+console.log(isSameType("3", 3)); // false
+
 
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
